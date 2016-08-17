51d12cba442086823d3fe39d9437a2098c7c47e9
Choldrim <choldrim@foxmail.com>
2016-08-17 10:39:57 +0800

    Support git storage target choice

--- main.py
+++ main.py
@@ -8,46 +8,52 @@ from lib.gitlab import Gitlab
 from lib.github import Github
 
 
-def new_project(proj_name, proj_type):
-    g = Gerrit()
-    print('\n--- gerrit ---')
-    code, msg = g.create_project(proj_name, proj_type)
+def new_project(proj_name, proj_type, git_target):
 
-    if not str(code).startswith('2'):
-        print(msg)
-        print('E: failed')
+    git_targets = git_target.split(',')
 
-    print('finish gerrit')
+    if 'gerrit' in git_targets:
+        g = Gerrit()
+        print('\n--- gerrit ---')
+        code, msg = g.create_project(proj_name, proj_type)
 
-    # github
-    print('\n--- github ---')
-    gh = Github()
-    code, msg = gh.create_project(proj_name)
+        if not str(code).startswith('2'):
+            print(msg)
+            print('E: failed')
 
-    if not str(code).startswith('2'):
-        print(msg)
-        print('E: failed')
+        print('finish gerrit')
 
-    print('finish github')
+    if 'github' in git_targets:
+        print('\n--- github ---')
+        gh = Github()
+        code, msg = gh.create_project(proj_name)
 
-    # gitlab
-    print('\n--- gitlab ---')
-    gl = Gitlab()
-    code, msg = gl.create_project(proj_name)
+        if not str(code).startswith('2'):
+            print(msg)
+            print('E: failed')
 
-    if not str(code).startswith('2'):
-        print(msg)
-        print('E: failed')
+        print('finish github')
 
-    print('finish gitlab')
+    if 'gitlab' in git_targets:
+        print('\n--- gitlab ---')
+        gl = Gitlab()
+        code, msg = gl.create_project(proj_name)
+
+        if not str(code).startswith('2'):
+            print(msg)
+            print('E: failed')
+
+        print('finish gitlab')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('name', help='project name')
     parser.add_argument('-t', '--type', help='project type [public | private | web]')
+    parser.add_argument('-g', '--git', help='choose one or multi target, [gerrit | github | gitlab], eg: github,gitlab')
     args = parser.parse_args()
     proj_name = args.name
     proj_type = args.type
+    git = args.git
 
-    new_project(proj_name, proj_type)
+    new_project(proj_name, proj_type, git)
